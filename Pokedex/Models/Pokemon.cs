37890d452f7a3cffc0892c4ff9d2ef9b996904f6
anas1306 <mohammed.anas1306@gmail.com>
2026-07-14 19:59:05 -0300

    Added a Model for Pokemon

---
+++ Pokedex/Models/Pokemon.cs
@@ -0,0 +1,28 @@
+namespace Pokedex.Models;
+
+public class Pokemon
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public List<PokemonType>? Types { get; set; }
+    public int Height { get; set; }
+    public int Weight { get; set; }
+    public Sprites? Sprites { get; set; }
+}
+
+public class Sprites
+{
+    public string? front_default { get; set; }
+}
+
+public class TypeInfo
+{
+    public string? name { get; set; }
+    public string? url { get; set; }
+}
+
+public class PokemonType
+{
+    public int slot { get; set; }
+    public TypeInfo? type { get; set; }
+}
