9e81b483c11c59560666ebe6ba61afdb3ad11467
anas1306 <mohammed.anas1306@gmail.com>
2026-07-21 09:27:41 -0300

    Added sprite image loaded from API, displayed pikachu

--- Pokedex/ViewModels/MainViewModel.cs
+++ Pokedex/ViewModels/MainViewModel.cs
@@ -2,6 +2,9 @@ using Pokedex.Services;
 using System.Threading.Tasks;
 using CommunityToolkit.Mvvm.ComponentModel;
 using Pokedex.Models;
+using Avalonia.Media.Imaging;
+using System.Net.Http;
+using System.IO;
 
 namespace Pokedex.ViewModels;
 
@@ -9,6 +12,8 @@ public partial class MainViewModel : ViewModelBase
 {
     [ObservableProperty]
     public partial Pokemon? SelectedPokemon { get; set; }
+    [ObservableProperty]
+    public partial Bitmap? SpriteImage { get; set; }
     public MainViewModel()
     {
         _ = LoadPokemon();
@@ -18,5 +23,10 @@ public partial class MainViewModel : ViewModelBase
     {
         Pokemon? pokemon = await new PokeApiService().GetPokemon("pikachu");
         SelectedPokemon = pokemon;
+        if (pokemon?.Sprites?.front_default != null) {
+            byte[] imageBytes = await new HttpClient().GetByteArrayAsync(pokemon.Sprites.front_default);
+            using var stream = new MemoryStream(imageBytes);
+            SpriteImage = new Bitmap(stream);
+        }   
     }
 }
